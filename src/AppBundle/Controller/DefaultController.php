299fa7dfc7ca3c55133011e3667d0275fe361397
rbartlettquotient <rbartlett@quotient-inc.com>
2018-04-18 10:34:15 -0400

    Fix to return issues in an array, keyed by 'files', as javascript is expecting.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,52 +8,68 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/bagit.html.twig');
+  /**
+   * @Route("/", name="homepage")
+   */
+  public function indexAction(Request $request)
+  {
+      return $this->render('default/bagit.html.twig');
+  }
+
+  /**
+   * @Route("/bagit/check", name="bagit-check")
+   */
+  public function checkerAction(Request $request)
+  {
+    $folderpath = $request->request->get('folderpath');
+
+    if(empty($folderpath)) {
+      $folderpath = $request->query->get('folderpath');
     }
 
-    /**
-     * @Route("/bagit/check", name="bagit-check")
-     */
-    public function checkerAction(Request $request)
-    {
-        $folderpath = $request->request->get('folderpath');
-        require $this->get('kernel')->getRootDir()."/../vendor/scholarslab/bagit/lib/bagit.php";
-        $checkfiles = $this->validatefolder($folderpath);
-        if (count($checkfiles) > 0) {
-            $bag = new \BagIt($folderpath);
-        }
-        if (isset($bagit)) {
-            $validation = $bag->validate();
-            if (count($validation) > 0) {
-                for ($i=0; $i < count($validation); $i++) { 
-                    foreach ($validation[$i] as $value) {
-                        if ($value == 'bagit.txt') {
-                            $bag->update();
-                        }
-                    }
-                }
-            }
-        }
-        return new JsonResponse($checkfiles);
-    }
-    /**
-     * @Route("/bagit/update", name="bagit-update")
-     */
-    public function updateAction(Request $request)
-    {
-        $folderpath = $request->request->get('folderpath');
-        require $this->get('kernel')->getRootDir()."/../vendor/scholarslab/bagit/lib/bagit.php";
-        $fileparts = pathinfo($folderpath);
+    require $this->get('kernel')->getRootDir()."/../vendor/scholarslab/bagit/lib/bagit.php";
+    $checkfiles = $this->validatefolder($folderpath);
 
-        $bag = new \BagIt($folderpath);
-        $bag->update();
-        return new JsonResponse(array("flag"=>true));
+    if (count($checkfiles['files']) == 0) {
+      $bag = new \BagIt($folderpath);
+      $validation = $bag->validate();
+
+      if (count($validation) > 0) {
+        foreach ($validation as $message) {
+          $full_message = '';
+          foreach ($message as $k => $value) {
+            $full_message .= ' ' . $value;
+          }
+          $checkfiles[] = $full_message;
+        }
+      }
+    }
+
+    if(count($checkfiles) > 0) {
+      $checkfiles = array('files' => $checkfiles);
+    }
+
+    return new JsonResponse($checkfiles);
+
+  }
+
+  /**
+   * @Route("/bagit/update", name="bagit-update")
+   */
+  public function updateAction(Request $request)
+  {
+      $folderpath = $request->request->get('folderpath');
+
+      if(empty($folderpath)) {
+        $folderpath = $request->query->get('folderpath');
+      }
+
+      require $this->get('kernel')->getRootDir()."/../vendor/scholarslab/bagit/lib/bagit.php";
+      //$fileparts = pathinfo($folderpath);
+
+      $bag = new \BagIt($folderpath);
+      $bag->update();
+      return new JsonResponse(array("flag"=>true));
 
     }
     function validatefolder($path){
